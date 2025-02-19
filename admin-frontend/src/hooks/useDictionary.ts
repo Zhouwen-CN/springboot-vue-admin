9d9e4a8276672afc3c1623c68aa63978ca1b47ff
Zhouwen-CN <597879949@qq.com>
2025-02-19 16:17:32 +0800

    feat(admin-frontend): 新增 useDictionary，有下拉列表的表单提交才会使用字典

---
+++ admin-frontend/src/hooks/useDictionary.ts
@@ -0,0 +1,37 @@
+import {type DictDataVo, reqGetDictDataListByTypeId} from '@/api/tool/dict'
+import {type Ref} from 'vue'
+
+export interface UseDictResult {
+    dictData: Ref<DictDataVo[]>
+    dictMap: Ref<Map<number, string>>
+    run: Function
+}
+
+/**
+ * 有下拉菜单表单提交，才会使用字典
+ * 像日志管理，有下拉列表，没有表单提交，这种最好不使用字典
+ * @param type 字典类型
+ * @returns 字典
+ */
+function useDict(type: string): UseDictResult {
+    const dictData = ref<DictDataVo[]>([])
+    const dictMap = ref<Map<number, string>>(new Map<number, string>())
+
+    function run() {
+        reqGetDictDataListByTypeId(type)
+            .then((res) => {
+                dictData.value = res.data
+
+                res.data.forEach((item) => {
+                    dictMap.value.set(item.value, item.label)
+                })
+            })
+            .catch((error) => {
+                console.warn(error)
+            })
+    }
+
+    return {dictData, dictMap, run}
+}
+
+export default useDict
