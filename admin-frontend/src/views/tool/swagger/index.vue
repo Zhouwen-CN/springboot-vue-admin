06b9920010282af4293b7f653bce9f1c77dd7b93
Zhouwen-CN <597879949@qq.com>
2025-04-27 09:49:39 +0800

    feat(admin-frontend): swagger-ui-dist改为swagger-ui，官网推荐这么做，能减少一点打包体积

--- admin-frontend/src/views/tool/swagger/index.vue
+++ admin-frontend/src/views/tool/swagger/index.vue
@@ -1,23 +1,15 @@
 <script setup>
-import 'swagger-ui-dist/swagger-ui.css'
-import {SwaggerUIBundle, SwaggerUIStandalonePreset} from "swagger-ui-dist"
+import 'swagger-ui/dist/swagger-ui.css'
+import SwaggerUI from 'swagger-ui'
 import request from '@/utils/request'
 
 // 不用iframe是因为不好做鉴权
 onMounted(async () => {
   const data = await request.get('/v3/api-docs')
-  SwaggerUIBundle({
+  SwaggerUI({
     spec: data,
     dom_id: '#swagger-ui',
-    docExpansion: 'none',
-    presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
-    layout: "StandaloneLayout"
+    docExpansion: 'none'
   })
 })
 </script>
